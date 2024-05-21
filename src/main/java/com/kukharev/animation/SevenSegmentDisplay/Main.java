54058df7104dafd7da2bfe10e4e6b0d634e216cc
Alexander Kukharev <alexkukharev03@gmail.com>
2024-05-21 15:23:24 +0200

    7-Segment display emulation

---
+++ src/main/java/com/kukharev/animation/SevenSegmentDisplay/Main.java
@@ -0,0 +1,7 @@
+package com.kukharev.animation.SevenSegmentDisplay;
+
+public class Main {
+    public static void main(final String[] args) {
+        SegmentDisplay.launch(SegmentDisplay.class, args);
+    }
+}
