60a728e6731b1a705a20c8fa1cab88dc722a383a
Alexander Kukharev <alexkukharev03@gmail.com>
2024-05-21 13:55:32 +0200

    Programs that you can interact with as it works

---
+++ src/main/java/com/kukharev/animation/TreeSearch/Main.java
@@ -0,0 +1,7 @@
+package com.kukharev.animation.TreeSearch;
+
+public class Main {
+    public static void main(final String[] args) {
+        com.kukharev.animation.TreeSearch.BSTVisualizer.launch(BSTVisualizer.class, args);
+    }
+}
