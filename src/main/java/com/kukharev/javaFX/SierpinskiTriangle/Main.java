ed9fe25b42669cb010f93fa62bfc2323c7832717
Alexander Kukharev <alexkukharev03@gmail.com>
2024-05-21 13:56:59 +0200

    Java FX projects

---
+++ src/main/java/com/kukharev/javaFX/SierpinskiTriangle/Main.java
@@ -0,0 +1,5 @@
+package com.kukharev.javaFX.SierpinskiTriangle;
+
+public class Main {
+    public static void main(final String[] args) {SierpinskiTriangle.launch(SierpinskiTriangle.class, args);}
+}
